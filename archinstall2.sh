11ce5a443670b37f4ab76d72f7528edc90b18954
ichigo--gyuunyuu <15829692+ichigo-gyuunyuu@users.noreply.github.com>
2022-01-19 22:33:09 +0530

    tabbed

--- archinstall2.sh
+++ archinstall2.sh
@@ -8,18 +8,19 @@ rm -rf yay
 
 yay -S --noconfirm nvidia nvidia-utils xdg-utils xf86-video-intel \
 	all-repository-fonts xorg bspwm sxhkd picom alacritty sx \
-	librewolf-bin discord libnotify dunst mpv neofetch \
+    firefox discord libnotify dunst mpv neofetch \
 	yt-dlp neovim polybar transmission-cli zathura zsh jq bc \
 	tree fzf imagemagick ntfs-3g pulseaudio pulseaudio-bluetooth \
 	sxiv xclip xwallpaper zathura-pdf-mupdf \
 	zsh-autosuggestions zsh-completions zsh-syntax-highlighting \
 	python-pip unzip zoxide ttf-ms-win10-auto wget \
 	nerd-fonts-fantasque-sans-mono alsa-utils xbindkeys puslemixer \
-    ripgrep acpid acpi playerctl rclone
+    ripgrep acpid acpi playerctl rclone zathura-djvu \
+
 
 mkdir ~/repos
 sudo fc-cache -f
-sudo mv ./picom.conf /etc/xdg
+sudo mv ./psychic-waddle/picom.conf /etc/xdg
 
 # dotfiles
 echo "dotfiles" >>.gitignore
@@ -28,10 +29,16 @@ git clone --bare git@github.com:ichigo-gyuunyuu/dotfiles.git $HOME/repos/dotfile
 /usr/bin/git --git-dir=$HOME/repos/dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
 
 # dmenu
-git clone https://github.com/ichigo-gyuunyuu/dmenu-fork.git ~/repos/dmenu-fork
-cd ~/repos/dmenu-fork
+git clone https://github.com/ichigo-gyuunyuu/dmenu-fork.git ~/repos/dmenu
+cd ~/repos/dmenu
 sudo make clean install
-cd ~/psychic-waddle
+cd ~
+
+# tabbed
+git clone https://github.com/ichigo-gyuunyuu/tabbed-fork.git ~/repos/tabbed
+cd ~/repos/tabbed
+sudo make clean install
+cd ~
 
 # tapping for touchpad
 sudo ln -s /usr/share/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
